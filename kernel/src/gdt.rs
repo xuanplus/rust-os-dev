07adb593d84e74bb1c7d09f9520fd1b5fc702a8c
xuanplus <xuanplus@outlook.com>
2024-02-06 14:22:25 +0800

    add interrupt and gdt

---
+++ kernel/src/gdt.rs
@@ -0,0 +1,56 @@
+use core::ptr::addr_of;
+use lazy_static::lazy_static;
+use x86_64::structures::gdt::SegmentSelector;
+use x86_64::structures::gdt::{Descriptor, GlobalDescriptorTable};
+use x86_64::structures::tss::TaskStateSegment;
+use x86_64::VirtAddr;
+
+pub const DOUBLE_FAULT_IST_INDEX: u16 = 0;
+
+lazy_static! {
+    static ref TSS: TaskStateSegment = {
+        let mut tss = TaskStateSegment::new();
+        tss.interrupt_stack_table[DOUBLE_FAULT_IST_INDEX as usize] = {
+            const STACK_SIZE: usize = 4096 * 5;
+            static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+            let stack_start = VirtAddr::from_ptr(unsafe { addr_of!(STACK) });
+            let stack_end = stack_start + STACK_SIZE;
+            stack_end
+        };
+        tss
+    };
+    static ref GDT: (GlobalDescriptorTable, Selectors) = {
+        let mut gdt = GlobalDescriptorTable::new();
+        let code_selector = gdt.add_entry(Descriptor::kernel_code_segment());
+        let data_selector = gdt.add_entry(Descriptor::kernel_data_segment());
+        let tss_selector = gdt.add_entry(Descriptor::tss_segment(&TSS));
+        (
+            gdt,
+            Selectors {
+                code_selector,
+                tss_selector,
+                data_selector,
+            },
+        )
+    };
+}
+
+struct Selectors {
+    code_selector: SegmentSelector,
+    data_selector: SegmentSelector,
+    tss_selector: SegmentSelector,
+}
+
+pub fn init() {
+    use x86_64::instructions::segmentation::{Segment, CS, DS, SS};
+    use x86_64::instructions::tables::load_tss;
+
+    GDT.0.load();
+    unsafe {
+        CS::set_reg(GDT.1.code_selector);
+        DS::set_reg(GDT.1.data_selector);
+        SS::set_reg(GDT.1.data_selector);
+        load_tss(GDT.1.tss_selector);
+    }
+}
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -1,12 +1,16 @@
 #![no_std]
+#![feature(abi_x86_interrupt)]
 
 pub mod framebuffer;
+pub mod interrupts;
+pub mod gdt;
 
 use bootloader_api::info::FrameBuffer;
 use bootloader_api::BootInfo;
-use framebuffer::init_framebuffer;
 
 pub fn init(boot_info: &'static mut BootInfo) {
     let fb_option: Option<&'static mut FrameBuffer> = boot_info.framebuffer.as_mut();
-    init_framebuffer(fb_option);
+    framebuffer::init_framebuffer(fb_option);
+    gdt::init();
+    interrupts::init();
 }
