fe9bf3107f54c11e24754c8a9c12c31096a0e409
xuanplus <xuanplus@outlook.com>
2024-02-06 14:34:06 +0800

    add memory

--- kernel/src/interrupts.rs
+++ kernel/src/interrupts.rs
@@ -1,7 +1,7 @@
 use crate::gdt;
 use crate::println;
 use lazy_static::lazy_static;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
@@ -11,6 +11,7 @@ lazy_static! {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); // new
         }
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -28,4 +29,17 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    // hlt_loop();
 }
--- kernel/src/lib.rs
+++ kernel/src/lib.rs
@@ -4,6 +4,7 @@
 pub mod framebuffer;
 pub mod interrupts;
 pub mod gdt;
+pub mod memory;
 
 use bootloader_api::info::FrameBuffer;
 use bootloader_api::BootInfo;
